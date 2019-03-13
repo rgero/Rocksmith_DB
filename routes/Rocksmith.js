278eebd3bd005bdf9946a3a7ae78c7aa151e256f
Roy Gero <roy.gero.ii@gmail.com>
2019-03-13 14:50:33 -0400

    Starting to implement the put statement so I can add stuff.

--- routes/Rocksmith.js
+++ routes/Rocksmith.js
@@ -4,8 +4,45 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
+    var songString = "";
     const songs = await Song.find().sort('artist');
     res.send(songs);
+})
+
+router.put('/', async (req, res) => {
+    var songs = req.body;
+    if (typeof(songs) === 'object' && songs.constructor === Array){
+        console.log("We got an array");
+        songs.forEach( async (song) => {
+            const { error } = validate(song);
+            if (error) return res.status(400).send(error.details[0].message);
+    
+            let newSong = new Song({
+                artist: song.artist,
+                name: song.name,
+                leadTuning: song.leadTuning,
+                rhythmTuning: song.rhythmTuning,
+                bassTuning: song.bassTuning,
+            })
+    
+            newSong = await newSong.save();
+        })
+    } else {
+        const { error } = validate(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+
+        let song = new Song({
+            artist: req.body.artist,
+            name: req.body.name,
+            leadTuning: req.body.leadTuning,
+            rhythmTuning: req.body.rhythmTuning,
+            bassTuning: req.body.bassTuning,
+        })
+
+        song = await song.save();
+    }
+
+    res.send("Ok");
 })
 
 module.exports = router;
